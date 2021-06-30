7caa7c4f02477dc1fd4ebd6103b4ed7e9b14c187
Paweł Łęczycki <pawel@msd.info.pl>
2021-06-30 11:45:29 +0200

    DIP

---
+++ SOLID/V_DIP.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.SOLID
+{
+    interface IMessage
+    {
+        void Send();
+    }
+
+    class SMS : IMessage
+    {
+        public string Number { get; set; }
+        public string Content { get; set; }
+
+        public void Send()
+        {
+            SendSMS();
+        }
+
+        public void SendSMS()
+        {
+            Console.WriteLine("Sending SMS...");
+        }
+    }
+
+    class MMS : IMessage
+    {
+        public string Number { get; set; }
+        public byte[] Content { get; set; }
+
+        public void Send()
+        {
+            SendMMS();
+        }
+
+        public void SendMMS()
+        {
+            Console.WriteLine("Sending MMS...");
+        }
+    }
+
+    class Mail : IMessage
+    {
+        public string Address { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public byte[] Attachments { get; set; }
+
+        public void Send()
+        {
+            SendMail();
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("Sending Mail...");
+        }
+    }
+
+    class Messenger
+    {
+        public IEnumerable<IMessage> Messages { get; set; }
+
+        public void SendMessage()
+        {
+            Messages.ToList().ForEach(x => x.Send());
+        }
+    }
+}
