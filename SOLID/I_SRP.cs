c61dd757fba1a8c10d4ed414aafab391158eed4e
Paweł Łęczycki <pawel@msd.info.pl>
2021-06-30 11:07:35 +0200

    SRP

---
+++ SOLID/I_SRP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.SOLID
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class Validators { 
+        public static bool ValidateEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
