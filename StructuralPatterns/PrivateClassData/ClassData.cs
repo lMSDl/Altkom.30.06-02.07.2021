c890b4f23f882ac348d0037793d2c3e0586a1308
Paweł Łęczycki <pawel@msd.info.pl>
2021-07-01 12:52:40 +0200

    PrivateClassData

---
+++ StructuralPatterns/PrivateClassData/ClassData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.StructuralPatterns.PrivateClassData
+{
+    public class ClassData
+    {
+        private string _a;
+        private int _b;
+        private float _c;
+
+        public ClassData(string a, int b, float c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public string GetA()
+        {
+            return _a;
+        }
+
+        public int GetB()
+        {
+            return _b;
+        }
+
+        public float GetC()
+        {
+            return _c;
+        }
+    }
+}
---
+++ StructuralPatterns/PrivateClassData/SomeClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPCSharp.StructuralPatterns.PrivateClassData
+{
+    public class SomeClass
+    {
+        private ClassData _classData;
+        //private string _a;
+        //private int _b;
+        //private float _c;
+
+        public SomeClass(string a, int b, float c)
+        {
+            _classData = new ClassData(a, b, c);
+        }
+
+        public string GetA()
+        {
+            return _classData.GetA();
+        }
+        public int GetB()
+        {
+            return _classData.GetB();
+        }
+        public float GetC()
+        {
+            return _classData.GetC();
+        }
+
+        public void SomeFunction()
+        {
+            //_a = "abc";
+        }
+    }
+}
